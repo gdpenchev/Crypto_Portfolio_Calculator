6ed0756962b4f24acf16e3078121c5d29aee3dec
gdpenchev <georgipenchev90@gmail.com>
2024-10-08 18:15:54 +0300

    added wwwroot into .csproj

--- CryptoPortfolioCalculator/obj/Debug/net8.0/CryptoPortfolioCalculator.AssemblyInfo.cs
+++ CryptoPortfolioCalculator/obj/Debug/net8.0/CryptoPortfolioCalculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CryptoPortfolioCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec3de83f9a99c0fa9e4fa16af7198f920a3593b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db7fcb2877d453a159c0a25416a4784bbca54699")]
 [assembly: System.Reflection.AssemblyProductAttribute("CryptoPortfolioCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CryptoPortfolioCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
